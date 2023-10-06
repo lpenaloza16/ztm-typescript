020ffc6a6dbba8b3bdd04dd48917018082cd8c6b
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-10-06 11:55:02 -0700

    my practice

--- demo/arithmetic/arithmetic.ts
+++ demo/arithmetic/arithmetic.ts
@@ -2,3 +2,52 @@
 import { strict as assert } from "assert";
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+const add = 1 + 1;
+
+assert.equal(add, 2);
+
+const sub = 2 - 1;
+
+assert.equal(sub, 1);
+
+const mul = 3 * 3;
+
+assert.equal(mul, 9);
+
+const div = 8 / 2;
+
+assert.equal(div, 4);
+
+const inf = 1 / 0;
+
+assert.equal(inf, Infinity);
+// best to avoid zero all together -> get weird edge cases
+
+const nan = 0 / 0;
+
+assert(isNaN(nan));
+
+const rem = 10 % 3;
+assert.equal(rem, 1);
+
+// define asset.equal
+
+const neg = -rem;
+
+const neg2 = -neg;
+
+assert.equal(neg2, 1);
+
+const exp = 2 ** 3;
+
+assert.equal(exp, 8);
+
+// decimals and scientific notation should work, maybe even hex; but big ints won't work.
+
+const abs = Math.abs(-5);
+assert.equal(abs, 5);
+
+const pi = Math.PI;
+
+// Typescript already has PI built-in
